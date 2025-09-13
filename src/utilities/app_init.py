e9714a39c07955c60ac14eccfae51aeeb856daca
Jin-Mach <ji82ma@seznam.cz>
2025-09-13 17:42:41 +0200

    Added check_icons method in FilesProvider to verify application icons. Added call to FilesProvider.check_icons in app_init method. Minor code adjustments.

--- src/utilities/app_init.py
+++ src/utilities/app_init.py
@@ -5,7 +5,7 @@ from src.utilities.support_provider import SupportProvider
 def app_init() -> bool:
     init_ok = False
     if SupportProvider.check_internet_connection():
-        init_methods = [FilesProvider.check_json_files()]
+        init_methods = [FilesProvider.check_json_files(), FilesProvider.check_icons()]
         if all(init_methods):
             init_ok = True
     return init_ok
--- src/utilities/files_provider.py
+++ src/utilities/files_provider.py
@@ -32,8 +32,27 @@ class FilesProvider:
                         with open(files_path.joinpath(file), "w", encoding="utf-8") as raw_json:
                             raw_json.write(json_file.text)
             status = True
-            return status
         except Exception as e:
             error_handler = ErrorHandler()
-            error_handler.write_show_exception(e, cancel_visible=True)
+            error_handler.write_show_exception(e, continue_visible=False, cancel_visible=True)
+        return status
+
+    @staticmethod
+    def check_icons() -> bool:
+        status = False
+        try:
+            required_files = ["neutralMoodButton_icon.png", "sadMoodButton_icon.png", "smileMoodButton_icon.png"]
+            raw_url = "https://raw.githubusercontent.com/Jin-Mach/MoodApp/main/icons/"
+            files_path = pathlib.Path(__file__).parents[2].joinpath("icons")
+            files_path.mkdir(parents=True, exist_ok=True)
+            for file in required_files:
+                file_path = files_path.joinpath(file)
+                if not file_path.exists() or file_path.stat().st_size == 0:
+                    icon_file = requests.get(f"{raw_url}{file}", timeout=5)
+                    with open(file_path, "wb") as raw_icon:
+                        raw_icon.write(icon_file.content)
+            status = True
+        except Exception as e:
+            error_handler = ErrorHandler()
+            error_handler.write_show_exception(e, continue_visible=False, cancel_visible=True)
         return status
